a288ce1225d633cb1ad5e7090ccc9f78888c2dc3
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-17 23:45:00 +0100

    commit

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
-import console from 'console';
 
 @Injectable()
 export class AuthenticationService {
@@ -22,46 +21,25 @@ export class AuthenticationService {
   //   }
 
   async loginUser(user: User) {
-    const salt = await bcrypt.genSalt();
-    const password = await bcrypt.hash(user.user_password, salt);
-    user.user_password = password;
-    // console.log(user.user_password);
     const value = await this.repository.findOne({
-      where: { user_email: user.user_email, user_password: user.user_password },
+      where: { user_email: user.user_email },
     });
-    if (!value) {
-      throw new NotFoundException();
+    if (value) {
+      bcrypt.compare(
+        value.user_password,
+        user.user_password,
+        function (error, isMatch) {
+          if (error) {
+            return error;
+          } else if (!isMatch) {
+            return new NotFoundException();
+          } else {
+            return value;
+          }
+        },
+      );
     } else {
-      //const state = await bcrypt.compare(password, value.user_password);
-      //if (state) {
-      //return value;
-      //} else {
-      //throw new NotFoundException();
-      //}
-      return value;
+      throw new NotFoundException();
     }
   }
-
-  //   async updateCategory(id: number, category: Category): Promise<Category> {
-  //     const value = await this.repository.findOne(id);
-  //     if (!value) {
-  //       throw new NotFoundException();
-  //     } else {
-  //       await this.repository.update(id, category);
-  //       return await this.getCategory(value.id_category);
-  //     }
-  //   }
-
-  //   async deleteCategory(id: number) {
-  //     const value = await this.repository.findOne(id);
-  //     if (!value) {
-  //       throw new NotFoundException();
-  //     } else {
-  //       value.delete_at = new Date(Date.now());
-  //       await this.repository.update(id, value);
-  //       return {
-  //         message: 'Successfully deleted',
-  //       };
-  //     }
-  //   }
 }
