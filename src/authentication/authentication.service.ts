4d688d11f03eee0c247e10a83239d3c40fdf98e7
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-19 18:49:02 +0100

    commit

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -12,37 +12,32 @@ export class AuthenticationService {
     private repository: Repository<User>,
   ) {}
 
-  async keepUser(id: number): Promise<User> {
-    const user = await this.repository.findOne(id, {
-      // relations: [
-      //   'favorites',
-      //   'favorites.wallpaper',
-      //   'ratings',
-      //   'ratings.wallpaper',
-      // ],
-    });
+  async keepUser(id: number) {
+    const user = await this.repository.findOne(id);
     if (user) {
       return user;
+      // return {
+      //   id_user: user.id_user,
+      //   user_name: user.user_name,
+      //   user_username: user.user_username,
+      //   user_email: user.user_email,
+      //   user_password: user.user_password,
+      //   user_phone: user.user_phone,
+      // };
     } else {
       throw new NotFoundException();
     }
   }
 
-  async loginUser(user: User): Promise<User> {
+  async loginUser(user: User) {
     const value = await this.repository.findOne({
       where: [
         { user_email: user.user_email },
         { user_username: user.user_username },
       ],
-      // relations: [
-      //   'favorites',
-      //   'favorites.wallpaper',
-      //   'ratings',
-      //   'ratings.wallpaper',
-      // ],
     });
     if (value) {
-      const isMatch = bcrypt.compare(value.user_password, user.user_password);
+      const isMatch = bcrypt.compare(user.user_password, value.user_password);
       if (isMatch) {
         return value;
       } else {
@@ -53,18 +48,11 @@ export class AuthenticationService {
     }
   }
 
-  async registerUser(user: User): Promise<User> {
+  async registerUser(user: User) {
     const salt = await bcrypt.genSalt();
     const password = await bcrypt.hash(user.user_password, salt);
     user.user_password = password;
     const value = await this.repository.save(user);
-    return await this.repository.findOne(value.id_user, {
-      // relations: [
-      //   'favorites',
-      //   'favorites.wallpaper',
-      //   'ratings',
-      //   'ratings.wallpaper',
-      // ],
-    });
+    return await this.repository.findOne(value.id_user);
   }
 }
